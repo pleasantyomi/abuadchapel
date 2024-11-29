265bced8fd52af836b9ca45759c9d81a6d461a82
Pleasant Yomi <110311997+pleasantyomi@users.noreply.github.com>
2024-11-29 14:38:43 +0100

    feat: bugs....

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
 export default function NotFound() {
-  return <></>;
+  return (
+    <>
+      <div className="grid items-center place-items-center w-full h-screen">
+        <Image
+          src="/images/404.png"
+          alt="404-not found"
+          width={750}
+          height={750}
+          className="lg:w-6/12 w-10/12"
+        />
+      </div>
+    </>
+  );
 }
--- components/ui/line.tsx
+++ components/ui/line.tsx
@@ -1,4 +1,8 @@
-export default function Line({ className }) {
+interface LineProps {
+  className?: string;
+}
+
+export default function Line({ className }: LineProps) {
   return (
     <div
       className={`${className} bg-primary-Stone w-full h-[1px] bg-opacity-50`}
